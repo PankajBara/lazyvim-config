899be607448c92089dfe2b917f0d7c37ad46998d
Pankaj <barapankaj2@gmail.com>
2026-09-03 22:53:48 +0530

    chore: automatic Neovim config sync 2026-09-03T22:53:48+05:30

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Cursive italics for code comments (needs a coding font with cursive italics)
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimStarted",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Comment", { italic = true })
+  end,
+})

96cea93804994924a28f99f0a65c4b248ea0c8af
Pankaj <barapankaj2@gmail.com>
2026-09-10 21:04:55 +0530

    chore: automatic Neovim config sync 2026-09-10T21:04:55+05:30

--- lua/snippets/cpp.lua
+++ lua/snippets/cpp.lua
@@ -1,9 +1,10 @@
-vim.snippets.add("cpp", {
-  {
-    trig = "main",
-    name = "main",
-    dscr = "main with fast I/O",
-    body = [[
+local function add_cpp_snippets()
+  vim.snippets.add("cpp", {
+    {
+      trig = "main",
+      name = "main",
+      dscr = "main with fast I/O",
+      body = [[
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,28 +16,41 @@ int main() {
     return 0;
 }
 ]],
-  },
-  {
-    trig = "fastio",
-    name = "fastio",
-    dscr = "fast I/O boilerplate",
-    body = [[
+    },
+    {
+      trig = "fastio",
+      name = "fastio",
+      dscr = "fast I/O boilerplate",
+      body = [[
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     $0]],
-  },
-  {
-    trig = "fori",
-    name = "fori",
-    dscr = "index for loop",
-    body = [[for (int ${1:i} = 0; ${1:i} < ${2:n}; ++${1:i}) {
+    },
+    {
+      trig = "fori",
+      name = "fori",
+      dscr = "index for loop",
+      body = [[for (int ${1:i} = 0; ${1:i} < ${2:n}; ++${1:i}) {
     $0
 }]],
-  },
-  {
-    trig = "vpai",
-    name = "vpai",
-    dscr = "vector<pair<int,int>>",
-    body = [[vector<pair<${1:int}, ${2:int}>> ${3:v};$0]],
-  },
+    },
+    {
+      trig = "vpai",
+      name = "vpai",
+      dscr = "vector<pair<int,int>>",
+      body = [[vector<pair<${1:int}, ${2:int}>> ${3:v};$0]],
+    },
+  })
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimStarted",
+  once = true,
+  callback = function()
+    if vim.snippets then
+      add_cpp_snippets()
+    else
+      vim.defer_fn(add_cpp_snippets, 0)
+    end
+  end,
 })

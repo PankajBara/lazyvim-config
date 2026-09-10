4023283d86112a0a4a4e199444c43a64606c5a83
Pankaj <barapankaj2@gmail.com>
2026-09-10 20:30:42 +0530

    chore: automatic Neovim config sync 2026-09-10T20:30:42+05:30

--- lua/plugins/snacks-dashboard.lua
+++ lua/plugins/snacks-dashboard.lua
@@ -12,25 +12,24 @@ return {
         pane_gap = 5,
         preset = {
           header = [[
-╭──────────────────────────────────────────────╮
-│                  N E O V I M                 │
-│            a calm place to build             │
-╰──────────────────────────────────────────────╯]],
+   󰣚
+ neovim
+ a calm place to build]],
         },
         sections = {
-          { section = "header", padding = { 1, 2 } },
-          { section = "keys", gap = 1, padding = { 0, 2 } },
+          { section = "header", padding = { 2, 2 } },
+          { section = "keys", gap = 1, padding = { 1, 2 } },
           {
             section = "projects",
             title = " Projects",
             icon = "󰉗 ",
             indent = 2,
-            padding = { 0, 1 },
+            padding = { 1, 1 },
             filter = function(dir)
               return dir:match("/%.git$") == nil
             end,
           },
-          { section = "recent_files", title = " Recent Files", icon = "󰈔 ", indent = 2, padding = { 0, 2 } },
+          { section = "recent_files", title = " Recent Files", icon = "󰈔 ", indent = 2, padding = { 1, 2 } },
           { section = "startup", icon = "󰄉 " },
         },
         keys = {
